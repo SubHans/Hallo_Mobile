c7b10b8654341acf45b1d4273dfa4be248b3b849
SubHans <indraprayoga933@gmail.com>
2020-09-08 13:31:26 +0700

    Hello Mobile

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.nfc.Tag;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MyApplication";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.i(TAG, "SALAH");
+
+    }
+}
